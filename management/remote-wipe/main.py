cfcadad81d03f6371ac93a401dfddf86eced513d
Peter Jose De Sousa <peter.de.sousa@canonical.com>
2024-09-25 08:13:38 +0100

    Add py 3.8 note

--- management/remote-wipe/main.py
+++ management/remote-wipe/main.py
@@ -27,6 +27,7 @@ def wipe(safety_on=True, cycles=10):
     block_devices = json.loads(subprocess.check_output("lsblk --json".split(" ")))
     crypts = []
     for block_device in block_devices["blockdevices"]:
+        # := Requires py >3.8 
         if crypt := search_for_crypt(block_device):
             crypts.append(crypt)
     for crypt in crypts:
